4245e96216d305cbdfe2e44c91cf4fb7d96847aa
qatsindio <jmleflet@gmail.com>
2014-07-07 22:33:31 +0100

    Initial commit

---
+++ Assets/Scripts/ShipController.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+// public member variables
+public var rotationSpeed : float;
+public var translationSpeed : float;
+
+public var leftImpulse : GameObject;
+public var rightImpulse : GameObject;
+
+function Start () {
+	leftImpulse.SetActive (false);
+	rightImpulse.SetActive (false);
+}
+
+function Update () {
+	var vAxis : float = Input.GetAxis ("Vertical");
+	var hAxis : float = - Input.GetAxis ("Horizontal");
+	
+	// Ship movement
+	transform.Rotate (0, 0, hAxis * rotationSpeed, Space.Self);
+	//transform.Translate (0, vAxis * translationSpeed, 0, Space.Self);
+	var forceVector : Vector2 = new Vector2 (0, vAxis * translationSpeed);
+	transform.rigidbody2D.AddRelativeForce (forceVector);
+	
+}
+
